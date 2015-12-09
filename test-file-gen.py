5dcca05073ca2d84463163207e1547df81a21960
Winston Cheong <cheong.winston1337@gmail.com>
2015-12-09 02:51:28 -0500

    Changed testfile generator to fix issues.
    * It wasn't using the old values and incrementing them.
    * I added a minimum time difference so that the datapoints are more spread apart.

--- test-file-gen.py
+++ test-file-gen.py
@@ -13,29 +13,29 @@ import random
 from datetime import date, time, datetime, timedelta
 from pretty import pprint
 
-def randomTimeDelta(maxmilli=100):
+
+# Global variables
+timestamp = datetime.now()
+alt = int(random.random() * 500)
+speed = int(random.random() * 50)
+data = []
+
+
+def randomTimeDelta(minmilli=200, maxmilli=1000):
     """Return a time increment"""
-    milli = int(round(random.random() * maxmilli))
+    milli = int(round(random.random() * (maxmilli-minmilli))) + minmilli
     return timedelta(milliseconds=milli)
 
 def randomDelta(maxdelta=5):
     return int(round(random.random() * maxdelta))
 
-def createDatapoint(timestamp=datetime.now(), alt=-100, speed=-100):
-    if alt is -100:
-        alt = int(random.random() * 500)
-    else:
-        alt += randomDelta()
-
-    if speed is -100:
-        speed = int(random.random() * 50)
-    else:
-        speed += randomDelta()
-
-    timestamp += randomTimeDelta()
-
-    return {"timestamp": str(timestamp), "alt": alt, "speed": speed}
+def createDatapoint(timestamp, alt, speed):
+    """ Return a tuple (timestamp, alt, speed) """
+    return (timestamp + randomTimeDelta(), alt+randomDelta(), speed+randomDelta())
 
 
-data = [ createDatapoint() for i in range(1000) ]
+for i in range(100000):
+    (timestamp, alt, speed) = createDatapoint(timestamp, alt, speed)
+    data.append({"timestamp": str(timestamp), "alt": alt, "speed": speed})
+
 print json.dumps(data, indent=4)
