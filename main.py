3ccbfe781d926efa058fc806268aed40d54e923b
Winston Cheong <cheong.winston1337@gmail.com>
2015-12-09 02:59:45 -0500

    Added a restructureToPeriods function.
    The function takes in the bare data (a list of datapoints) and outputs a
    list of periods (which are that list of data points, but separated into
    groups of 1 minute)

--- main.py
+++ main.py
@@ -3,8 +3,12 @@
 import json
 import os.path
 import sys
+from pretty import pprint
 
 import module
+from datetime import timedelta, datetime
+
+datetimeformat = "%Y-%m-%d %H:%M:%S.%f" # 2015-12-09 01:18:41.891210
 
 def importJSONFile(filename):
     '''Imports data from file specified in commandline argument 1
@@ -19,7 +23,60 @@ def callModule(data):
     Parameters:
         data - Dictionary containing other parameters
     '''
-    module.classifyPhaseOfFlight(data)
+    #module.classifyPhaseOfFlight(data)
+    #print module.fiveNumberSummary([])
+    pprint(restructureToPeriods(data))
+
+
+
+
+def restructureToPeriods(data):
+    '''Restructure the data so that it consists of one minute intervals
+    Input python object structure:
+    [
+        {"timestamp": 2000/12/12 12:3456, alt: 300, "speed": 95 },
+        {"timestamp": 2000/12/12 12:3458, alt: 300, "speed": 95 },
+        {"timestamp": 2000/12/12 12:3702, alt: 300, "speed": 95 },
+        {"timestamp": 2000/12/12 12:3704, alt: 300, "speed": 95 },
+        ...
+    ]
+
+    Output python object structure:
+    [
+        [
+            {"timestamp": 2000/12/12 12:34:56, alt: 300, "speed": 95 },
+            {"timestamp": 2000/12/12 12:34:58, alt: 300, "speed": 95 },
+            ...
+        ],
+        [
+            {"timestamp": 2000/12/12 12:3702, alt: 300, "speed": 95 },
+            {"timestamp": 2000/12/12 12:3704, alt: 300, "speed": 95 },
+            ...
+        ],
+        ...
+    ]
+
+    '''
+    restructured = []
+    periodStartTime = datetime.strptime(data[0]["timestamp"], datetimeformat)
+    print periodStartTime
+
+    temp = []
+    for i in data:
+        # When the time difference is greater than one minute,
+        # we have our period, and start constructing another
+        if  (periodStartTime + timedelta(seconds=60)) < datetime.strptime(i["timestamp"], datetimeformat):
+            restructured.append(temp)
+            temp = []
+            periodStartTime = datetime.strptime(i["timestamp"], datetimeformat)
+        # append the datapoint regardless 
+        temp.append(i)
+
+    # Exiting the loop, there may be some left over datapoints.
+    if temp:
+        restructured.append(temp)
+
+    return restructured
 
 # START OF SCRIPT
 if len(sys.argv) >= 2:
