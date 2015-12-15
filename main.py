161c6da09a2fe8d331adbaa20272e1876cade8a8
John Bucknam <jbuck594@gmail.com>
2015-12-14 23:18:38 -0500

    Updated module to create periods.
    Need to update algorithms to take in one period to find one time.

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ datetimeformat = "%Y-%m-%d %H:%M:%S.%f" # 2015-12-09 01:18:41.891210
 
 def importJSONFile(filename):
     '''Imports data from file specified in commandline argument 1
+
+    Parameters:
+        filename - Name of JSON file to be loaded.
     '''
     data = json.load(file(filename))
     # pprint(data)
@@ -21,54 +24,13 @@ def callModule(data):
     '''Calls module file
 
     Parameters:
-        data - Dictionary containing other parameters
+        data - JSON file containing information from aircraft.
     '''
-    print module.phaseClassification(data)
+    print module.phaseClassification(importJSONFile(data))
     # pprint(restructureToPeriods(data))
-
-
-
-
-def restructureToPeriods(data):
-    '''Restructure the data so that it consists of one minute intervals
-    Input python object structure:
-    [
-        { "timestamp": "2015-12-09 02:42:45.107267", "alt": 87, "speed": 16 }, 
-        { "timestamp": "2015-12-09 02:42:46.101267", "alt": 91, "speed": 21 }, 
-    ]
-
-
-    Output python object structure:
-    [
-        1 : 
-            [
-            ("2015-12-09 02:42:45.107267", 87, 16), ("2015-12-09 02:42:46.101267", 91, 21), ...
-            ], 
-        2: ...
-    ]
-    '''
-
-    restructured = []
-    periodStartTime = datetime.strptime(data[0]["timestamp"], datetimeformat)
-
-    temp = []
-    for i in data:
-        # When the time difference is greater than one minute,
-        # we have our period, and start constructing another
-        if  (periodStartTime + timedelta(seconds=60)) < datetime.strptime(i["timestamp"], datetimeformat):
-            restructured.append(temp)
-            temp = []
-            periodStartTime = datetime.strptime(i["timestamp"], datetimeformat)
-        # append the datapoint regardless 
-        temp.append(tuple(i.values()))
-
-    # Exiting the loop, there may be some left over datapoints.
-    if temp:
-        restructured.append(temp)
-
-    return restructured
+    print module.ruleClassification(importJSONFile(data))
 
 # START OF SCRIPT
 if len(sys.argv) >= 2:
     if os.path.isfile(sys.argv[1]) and sys.argv[1].endswith('.json'):
-        callModule(restructureToPeriods(importJSONFile(sys.argv[1])))
+        callModule(sys.argv[1])
--- module.py
+++ module.py
@@ -16,14 +16,19 @@ def fiveNumberSummary(lst):
             "avg": sum(lst) / len(lst)
             }
 
-def phaseClassification(periods):
+def phaseClassification(correlatedData, time):
     '''Classify each period's phase of flight, and return this list.
 
     Parameters:
-        periods - Contains the altitudes and speeds for each period.
-                  List of Lists of Dictionaries 
+        correlatedData - Data containing details on
+                            Timestamps, Altitude, and Speed.
+        time - Timestamp of current time being seeked.
     '''
 
+    # Set create periods
+    periods = restructureDataToPeriods(correlatedData, time)
+
+    # Start process
     phases = []
 
     for period in periods:
@@ -39,28 +44,90 @@ def phaseClassification(periods):
         # Check for weird things like if the max is in the middle, and aircraft goes up and back down
 
         # If there is little altitude difference
-        if abs(altitude_summary["max"] - altitude_summary["min"]) < 2:
-            #If low speed
-            if speed_summary["avg"] <= 10: 
-                phases.append("taxing")
-            else: 
-                phases.append("cruising")
+        if abs(altitude_summary["max"] - altitude_summary["min"]) < 5:
+            #If low speed and low altitude
+            if speed_summary["avg"] <= 10 and altitude_summary["max"] <= 5: 
+                phases.append("Taxi")
+            # High altitude
+            else if altitude_summary["max"] > 5: 
+                phases.append("Cruise")
+            else:
+                phases.append("Unknown")
         else: # Otherwise, large altitude difference
-
             # Note, this may not be a reliable indicator of whether aircraft is ascending or descending
             if altitude_summary["first"] < altitude_summary["last"]:
-                phases.append("ascending")
+                phases.append("Ascend")
+            else if altitude_summary["first"] > altitude_summary["last"]:
+                phases.append("Descend")
             else:
-                phases.append("descending")
+                phases.append("Unknown")
 
-        #print altitude_summary, speed_summary
+        # print altitude_summary, speed_summary
 
     return phases
 
-def ruleClassification(periods):
+def ruleClassification(correlatedData, time):
     '''Classifies the rule of flight
 
     Parameters:
-        periods - Contains the altitudes, speeds, and rule of flight (if any) for each period.
-                  Dictionary of Lists (or List of Dictionary of Lists if multiple periods)
+        correlatedData - Data containing details on
+                            Timestamps, Altitude, and Speed.
+        time - Timestamp of current time being seeked.
     '''
+
+    # Set create periods
+    periods = restructureDataToPeriods(correlatedData, time)
+
+    # Start process
+    rules = []
+    
+    for period in periods:
+        (timestamps, altitudes, speeds, flightrules) = zip(*period)
+
+        plt.plot(altitudes, 'ro')
+        plt.show()
+        
+        
+
+def restructureToPeriods(data, time):
+    '''Restructure the data so that it consists of one minute intervals
+    Input python object structure:
+    [
+        { "timestamp": "2015-12-09 02:42:45.107267", "alt": 87, "speed": 16 }, 
+        { "timestamp": "2015-12-09 02:42:46.101267", "alt": 91, "speed": 21 }, 
+    ]
+
+
+    Output python object structure:
+    [
+        1 : 
+            [
+            ("2015-12-09 02:42:45.107267", 87, 16), ("2015-12-09 02:42:46.101267", 91, 21), ...
+            ], 
+        2: ...
+    ]
+
+    Parameters:
+        data - Data containing information on aircraft.
+        time - Timestamp of required information
+    '''
+
+    restructured = []
+    periodStartTime = datetime.strptime(data[0]["timestamp"], datetimeformat)
+
+    temp = []
+    for i in data:
+        # When the time difference is greater than one minute,
+        # we have our period, and start constructing another
+        if  (periodStartTime + timedelta(seconds=60)) < datetime.strptime(i["timestamp"], datetimeformat):
+            restructured.append(temp)
+            temp = []
+            periodStartTime = datetime.strptime(i["timestamp"], datetimeformat)
+        # append the datapoint regardless 
+        temp.append(tuple(i.values()))
+
+    # Exiting the loop, there may be some left over datapoints.
+    if temp:
+        restructured.append(temp)
+
+    return restructured
